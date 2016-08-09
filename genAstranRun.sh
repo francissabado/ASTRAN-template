1eba960f61a48e7aa76cf8a797b4f5f72389fc05
Francis Sabado <fs8080@gmail.com>
2016-08-09 18:34:29 -0500

    Working translation as well as lower case for netlist file

--- genAstranRun.sh
+++ genAstranRun.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
-
-
 #Get Current Directory Snippet
 #http://stackoverflow.com/questions/59895/can-a-bash-script-tell-which-directory-it-is-stored-in
 SOURCE="${BASH_SOURCE[0]}"
@@ -10,20 +8,19 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   SOURCE="$(readlink "$SOURCE")"
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )" 
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
+CELLNAME="TH24_A"
 PROJECTDIR="$(echo $DIR)"
+LOGFILE="${PROJECTDIR}/history.log"
+RULESFILE="${PROJECTDIR}/Chartered130.rul"
+NETLISTFILE="${PROJECTDIR}/$(echo $CELLNAME | tr A-Z a-z )_ASTRAN.sp"
 
-
-echo $PROJECTDIR
-
-
-
-
-
-
-
-
-
-
-
+# Change the template
+sed "
+s#<LOGFILE>#$(echo $LOGFILE)#g
+s#<RULESFILE>#$(echo $RULESFILE)#g
+s#<PROJECTDIR>#$(echo $PROJECTDIR)#g
+s#<CELLNAME>#$(echo $CELLNAME)#g
+s#<NETLISTFILE>#$(echo $NETLISTFILE)#g
+" "$@"
