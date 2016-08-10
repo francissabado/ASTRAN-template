96cba111c6e060946eed5d84fdc5dde5d1945301
Francis Sabado <fs8080@gmail.com>
2016-08-09 20:28:22 -0500

    Updated genAstranRun.sh to separate cell for each run

--- genAstranRun.sh
+++ genAstranRun.sh
@@ -1,4 +1,15 @@
-#!/bin/bash -e
+#!/bin/bash
+
+#-e Not compatible with large print out
+
+
+if [ "$#" -ne 1 ];
+then
+    echo "Usage: ./genAstranRun.sh <CELLNAME>"
+    exit 0
+fi
+
+mkdir -p ./"${1}"
 
 #Get Current Directory Snippet
 #http://stackoverflow.com/questions/59895/can-a-bash-script-tell-which-directory-it-is-stored-in
@@ -10,11 +21,17 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-CELLNAME="TH24_A"
-PROJECTDIR="$(echo $DIR)"
+CELLNAME="$(echo $1)"
+PROJECTDIR="$(echo $DIR)/${1}"
 LOGFILE="${PROJECTDIR}/history.log"
 RULESFILE="${PROJECTDIR}/Chartered130.rul"
 NETLISTFILE="${PROJECTDIR}/$(echo $CELLNAME | tr A-Z a-z )_ASTRAN.sp"
+
+
+#Copies requirements
+cp "$DIR/Chartered130.rul" "${PROJECTDIR}/"
+mkdir -p "${PROJECTDIR}/Layouts"
+
 
 # Change the template
 sed "
@@ -23,4 +40,7 @@ s#<RULESFILE>#$(echo $RULESFILE)#g
 s#<PROJECTDIR>#$(echo $PROJECTDIR)#g
 s#<CELLNAME>#$(echo $CELLNAME)#g
 s#<NETLISTFILE>#$(echo $NETLISTFILE)#g
-" "$@"
+" "${DIR}/astranRun.run" > "$PROJECTDIR/astranRun_${CELLNAME}.run"
+
+
+# mkdir -p "${DIR}/Netlist"
